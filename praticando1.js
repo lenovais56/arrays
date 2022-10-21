9f72e38ea394a3b07af856d8d21e33f99b3658e6
Leandro Novais <90646451+lenovais56@users.noreply.github.com>
2022-10-21 11:57:03 -0300

    Criando e utilizando array e utilizando metodos de array

---
+++ praticando1.js
@@ -0,0 +1,21 @@
+const numero1 = 50;
+const numero2 = 43;
+const numero3 = 12;
+
+// utilizando array com 3 elementos
+const numeros = [50, 43, 12];
+
+const somaNumeros = (numeros[0] + numeros[1] + numeros[2])
+const multiplicaNumeros = (numeros[0] * numeros[1] * numeros[2])
+const mediaNumeros = (numeros[0] + numeros[1] + numeros[2]) / numeros.length; // .length especifica o tamanho de um array
+
+console.log(somaNumeros)
+console.log(multiplicaNumeros)
+console.log(mediaNumeros)
+
+const arrayDeNumeros = [31, 01, 16];
+const arrayDeStrings = ["morango", "alura", "Leandro"];
+
+console.log(arrayDeNumeros);
+console.log(arrayDeStrings);
+console.log(arrayDeStrings.length);
---
+++ readme.md
@@ -0,0 +1,24 @@
+# <h1 align="center" font-size="bold"> Curso Javascript - Arrays </h1>
+
+## 🔨 Principais aprendizados
+
+- `1`: Criar e utlizar array com elementos (numbers e strings)
+- `2`: Utilizar métodos de array do JS (ex.: .push)
+- `3`: 
+- `4`:
+- `5`:
+
+## ✔️ Tecnologias utilizadas
+
+- ``JavaScript``
+- ``NodeJS``
+- ``Git``
+- ``GitHub``
+- ``VS code``
+
+## 💻 Link do curso
+
+https://cursos.alura.com.br/course/fundamentos-javascript-arrays
+
+## 📃 Link do certificado
+
